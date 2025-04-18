61c691d6aa554e30d68d98e3cdbdb8e10f1991f8
Anwar Rahim <anwargithub@gmail.com>
2025-04-18 14:59:22 +0500

    Refactor sign-up form: update employment status radio buttons and fix form action

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+
 import Form from './components/Form'
 
 import './App.css'
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,36 +1,47 @@
+
+
 export default function Form(){
 
-    
 
+function signUp(formData){
+    
+    const email = formData.get("email")
+    const password = formData.get("password")
+    const descriptionJob = formData.get("description")
+    const employmentStatus = formData.get("employment-status")
+    console.log(employmentStatus)
+
+}
 
 
     return(
         <section>
 
             <h1>Sign Up form</h1>
-            <form action="signUp">  
+            <form action={signUp} >  
                 <label htmlFor="email">Email</label>
                 <input id="email" type="email" placeholder="hello@anwarrahim.com" name="email" />
                 <label htmlFor="password">Password</label>
                 <input id="password" type="password" placeholder="Er*****7@" name="password" />
                 <label htmlFor="description"> Description:</label>
-                <textarea id="description" placeholder="I am Frontend Engineer..." name="description"></textarea>
+                <textarea id="description" defaultValue={"I am doing freelance job on Upwork"} placeholder="I am Frontend Engineer..." name="description"></textarea>
             <fieldset>
                 <legend>Employment status:</legend>
                 <label>
-                <input type="radio" id="unemployed" name="employment-status"/>   
+                <input type="radio" id="unemployed" name="employment-status" value="unemployed"/>   
                 Unemployed
                 </label>
                 <label >
-                   <input type="radio" name="employment-status" />
+                   <input type="radio" name="employment-status" value="Part-Time" />
                    Part-Time
                 </label>
                 <label >
-                    <input type="radio" name="employment-status" />
+                    <input type="radio" name="employment-status" value="Full-Time" />
                     Full-Time
                 </label>
-
             </fieldset>
+
+            <button>Submit</button>
 
             </form>
             
